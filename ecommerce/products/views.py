974a8f126df4cf2f88e19587fc658a1054839185
Aditya Bansal <maddymanu@hotmail.com>
2014-08-23 17:14:26 +0530

    Product List on Homepage working.
    Add images man.

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -5,13 +5,8 @@ from .models import Product
 
 def home(request):
 
-  if request.user.is_authenticated():
-    # can use dictionaries to add variables for templates here
-    username_is = "Adi using contect"
-    context = {"username_is" : request.user}
-  else:
-    context = {"username_is" : "unknown"}
-
+  products = Product.objects.all()
+  context = {"products" : products}
   template = 'products/home.html'
   return render(request , template, context)
 
