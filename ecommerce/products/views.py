b2431b557972917ace9438429665a4bb7672e4e8
Aditya Bansal <maddymanu@hotmail.com>
2014-08-24 11:52:26 +0530

    Product search complete.

--- ecommerce/products/views.py
+++ ecommerce/products/views.py
@@ -3,8 +3,22 @@ from django.shortcuts import render, Http404
 # Create your views here.
 from .models import Product
 
-def home(request):
 
+def search(request):
+  try:
+    q= request.GET.get("q")
+  except: q=None
+  if q:
+    products = Product.objects.filter(title__icontains=q)
+    template = 'products/results.html'
+    context = {"query" : q , "products":products}
+  else:
+    context = {}
+    template = 'products/home.html'
+  return render(request , template, context)
+
+
+def home(request):
   products = Product.objects.all()
   context = {"products" : products}
   template = 'products/home.html'
