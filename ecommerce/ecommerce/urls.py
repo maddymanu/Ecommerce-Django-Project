5e8f4095f520916ec248709741c2aa4810366c6e
Aditya Bansal <maddymanu@hotmail.com>
2014-08-25 11:25:13 +0530

    Updating Cart works.

--- ecommerce/ecommerce/urls.py
+++ ecommerce/ecommerce/urls.py
@@ -11,6 +11,7 @@ urlpatterns = patterns('',
     url(r'^products/$', 'products.views.all', name='products'),
     url(r'^products/(?P<slug>[\w-]+)/$', 'products.views.single', name='single_product'),
     url(r'^cart/$', 'carts.views.view', name='cart'),
+    url(r'^cart/(?P<slug>[\w-]+)/$', 'carts.views.update_cart', name='update_cart'),
 
 
     # url(r'^blog/', include('blog.urls')),
