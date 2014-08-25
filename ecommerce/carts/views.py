271bd0b6e4f63d2a3be829c1c233bd8d1dcc3f4c
Aditya Bansal <maddymanu@hotmail.com>
2014-08-25 12:36:26 +0530

    Sessions work. IMPORTANT.

--- ecommerce/carts/views.py
+++ ecommerce/carts/views.py
@@ -7,13 +7,34 @@ from .models import Cart
 
 
 def view(request):
-  cart = Cart.objects.all()[0]
+  try:
+    the_id = request.session['cart_id']
+  except:
+    the_id = None
+
+  if the_id:
+    cart = Cart.objects.get(id=the_id)
+    context = {"cart" : cart}
+  else:
+    empty_message = "Your cart is empty"
+    context = {"empty": True , "empty_message" : empty_message}
+
   template= 'cart/view.html'
-  context = {"cart" : cart}
   return render(request, template, context)
 
 def update_cart(request, slug):
-  cart = Cart.objects.all()[0]
+  # expiration for the current session.
+  request.session.set_expiry(120000)
+  try:
+    the_id = request.session['cart_id']
+  except:
+    new_cart = Cart()
+    new_cart.save()
+    request.session['cart_id'] = new_cart.id
+    the_id = new_cart.id
+
+  cart = Cart.objects.get(id=the_id)
+
   try:
     product = Product.objects.get(slug=slug)
   except Product.DoesNotExist:
@@ -29,6 +50,7 @@ def update_cart(request, slug):
   new_total = 0.00
   for item in cart.products.all():
     new_total += float(item.price)
+  request.session['items_total'] = cart.products.all.count()
   cart.total = new_total
   cart.save()
   return HttpResponseRedirect(reverse("cart"))
